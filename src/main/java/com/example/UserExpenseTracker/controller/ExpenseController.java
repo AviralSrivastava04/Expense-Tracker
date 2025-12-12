47ce8dbe2d49872dda3bd3ef1a4b1d1e0702b3d9
Aviral Srivastava <sriaviral44@gmail.com>
2025-12-12 09:49:07 -0800

    AddExpense Api Created

--- src/main/java/com/example/UserExpenseTracker/controller/ExpenseController.java
+++ src/main/java/com/example/UserExpenseTracker/controller/ExpenseController.java
@@ -1,11 +1,11 @@
 package com.example.UserExpenseTracker.controller;
 
+import com.example.UserExpenseTracker.dto.ExpenseInputDto;
 import com.example.UserExpenseTracker.dto.ExpenseOutputDto;
 import com.example.UserExpenseTracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -15,8 +15,23 @@ public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
 
-    @GetMapping("/getExpense")
-    public ResponseEntity<List<ExpenseOutputDto>> getExpense(){
-        return ResponseEntity.ok(expenseService.getExpense());
+//    @GetMapping("/getExpense")
+//    public ResponseEntity<List<ExpenseOutputDto>> getExpense(){
+//        return ResponseEntity.ok(expenseService.getExpense());
+//    }
+
+    @GetMapping("/getExpense/{id}")
+    public ResponseEntity<ExpenseOutputDto> getExpenseById(@PathVariable Long id){
+        return ResponseEntity.ok(expenseService.getExpenseById(id));
+    }
+
+    @PostMapping("/addExpense")
+    public ResponseEntity<String> addExpense(@RequestBody ExpenseInputDto expenseInputDto){
+        return ResponseEntity.ok(expenseService.addExpense(expenseInputDto));
+    }
+
+    @GetMapping("/totalExpense")
+    public ResponseEntity<String> getTotalExpense(){
+        return ResponseEntity.ok(expenseService.getTotalAmt());
     }
 }
--- src/main/java/com/example/UserExpenseTracker/service/ExpenseService.java
+++ src/main/java/com/example/UserExpenseTracker/service/ExpenseService.java
@@ -1,10 +1,14 @@
 package com.example.UserExpenseTracker.service;
 
+import com.example.UserExpenseTracker.dto.ExpenseInputDto;
 import com.example.UserExpenseTracker.dto.ExpenseOutputDto;
+import com.example.UserExpenseTracker.entity.Expense;
 import com.example.UserExpenseTracker.repository.ExpenseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -12,6 +16,35 @@ public class ExpenseService {
     @Autowired
     private ExpenseRepo expenseRepo;
 
-    public List<ExpenseOutputDto> getExpense() {
+    public String getTotalAmt() {
+        List<Expense> expenseList = expenseRepo.findAll();
+        Double totalAmt = 0.0;
+        for(Expense expense : expenseList){
+            totalAmt = totalAmt + expense.getAmount();
+        }
+        return "Rs." + totalAmt;
+    }
+
+    public ExpenseOutputDto getExpenseById(Long id){
+        Expense expense = expenseRepo.findById(id).orElse(null);
+        ExpenseOutputDto expenseOutputDto = new ExpenseOutputDto();
+        expenseOutputDto.setExpenseName(expense.getExpenseName());
+        expenseOutputDto.setPaymentMode(expense.getPaymentMode());
+        expenseOutputDto.setId(expense.getId());
+        expenseOutputDto.setDate(expense.getDate());
+        expenseOutputDto.setAmount(expense.getAmount());
+
+        return expenseOutputDto;
+    }
+
+    public String addExpense(ExpenseInputDto expenseInputDto){
+        Expense expense = new Expense();
+        expense.setExpenseName(expenseInputDto.getExpenseName());
+        expense.setDate(LocalDate.now());
+        expense.setAmount(expenseInputDto.getAmount());
+        expense.setPaymentMode(expenseInputDto.getPaymentMode());
+
+        expenseRepo.save(expense);
+        return "Expense Added!!!";
     }
 }
